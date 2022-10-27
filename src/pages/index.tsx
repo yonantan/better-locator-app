016ba7745d7c69b148bf3648140f167db8546242
yonantan <yonantan@users.noreply.github.com>
2022-10-27 11:39:40 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { useState } from "react";
 import useGeolocation from "react-hook-geolocation";
 import { trpc } from "../utils/trpc";
